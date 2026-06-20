62bdd0cd84a86cb8f5eb971663e46330d4cc38a7
yilhuang <hyl88369391@163.com>
2026-06-20 23:54:26 +0800

    update gilneas, bastion, baradin

--- DXE_Baradin/Data_Baradin_Gilneas.lua
+++ DXE_Baradin/Data_Baradin_Gilneas.lua
@@ -6,6 +6,117 @@ DXE:RegisterRealmPatch(realm, "argaloth", {
     windows = {
         proxwindow = false,
     },
+    alerts = {
+        meteorcd = {
+            varname = format(L.alert["%s CD"],SN[45150]),
+            text = format(L.alert["Next %s"],SN[45150]),
+            time = 16.5,
+            icon = ST[45150],
+        },
+        meteorwarn = {
+            varname = format(L.alert["%s Warning"],SN[45150]),
+            text = "<meteortext>",
+            time = 3,
+            icon = ST[45150],
+        },
+        -- Fel Firestorm
+        firestormwarn = {
+            varname = format(L.alert["%s Warning"],SN[88972]),
+            text = format(L.alert["%s"],SN[88972]),
+            time = 3,
+            icon = ST[88972],
+        },
+        firestormdur = {
+            varname = format(L.alert["%s Duration"],SN[88972]),
+            text = format(L.alert["%s"],SN[88972]),
+            time = 18,
+            flashtime = 5,
+            icon = ST[88972],
+        },
+        consumingcd = {
+            varname = format(L.alert["%s CD"],SN[88954]),
+            text = format(L.alert["Next %s"],SN[88954]),
+            time = 22,
+            flashtime = 5,
+            icon = ST[88954],
+        },
+    },  
+    events = {
+    -- Consuming Darkness
+        {
+            type = "combatevent",
+            eventtype = "SPELL_AURA_APPLIED",
+            spellname = 95173,       
+            execute = {
+                {
+                "quash","consumingcd",
+                "alert","consumingcd",
+                },
+            },              
+        },
+        {
+            type = "combatevent",
+            eventtype = "SPELL_AURA_APPLIED",
+            spellname = 88954,
+            execute = {
+                {
+                "quash","consumingcd",
+                "alert","consumingcd",
+                },
+            },              
+        },
+        -- Meteor Slash
+        {
+            type = "combatevent",
+            eventtype = "SPELL_CAST_START",
+            spellname = 45150,
+            execute = {
+                {
+                    "quash","meteorcd",
+                    "alert","meteorcd",
+                },
+            },
+        },
+        {
+            type = "combatevent",
+            eventtype = "SPELL_AURA_APPLIED",
+            spellname = 88942,
+            execute = {
+                {
+                    "expect",{"#4#","==","&playerguid&"},
+                    "set",{meteortext = format("%s (%s) on %s",SN[88942],"1",L.alert["YOU"])},
+                    "alert","meteorwarn",
+                },
+            },
+        },
+        {
+            type = "combatevent",
+            eventtype = "SPELL_AURA_APPLIED_DOSE",
+            spellname = 88942,
+            execute = {
+                {
+                    "expect",{"#4#","==","&playerguid&"},
+                    "set",{meteortext = format("%s (%s) on %s",SN[88942],"#11#",L.alert["YOU"])},
+                    "alert","meteorwarn",
+                },
+            },
+        },
+        
+        
+        -- Fel Firestorm
+        {
+            type = "combatevent",
+            eventtype = "SPELL_CAST_START",
+            spellname = 88972,
+            execute = {
+                {
+                    "quash","meteorcd",
+                    "alert","firestormdur",
+                    "alert","firestormwarn",
+                },
+            },
+        },
+    }, 
 })
 
 DXE:RegisterRealmPatch(realm, "occuthar", {
--- DXE_Bastion/Data_Bastion_JRG.lua
+++ DXE_Bastion/Data_Bastion_JRG.lua
@@ -1,2 +1,39 @@
 -- Data_Bastion_JRG.lua
--- placeholder
+local L, SN, ST = DXE.L, DXE.SN, DXE.ST
+local realm = "JRG"
+
+DXE:RegisterRealmPatch(realm, "halfus", {
+    windows = {
+        proxwindow = false,
+    },
+})
+
+DXE:RegisterRealmPatch(realm, "valther", {
+    windows = {
+        proxwindow = false,
+    },
+})
+
+DXE:RegisterRealmPatch(realm, "ascendcouncil", {
+    windows = {
+        proxwindow = false,
+    },
+})
+
+DXE:RegisterRealmPatch(realm, "chogall", {
+    windows = {
+        proxwindow = false,
+    },
+})
+
+DXE:RegisterRealmPatch(realm, "sinestra", {
+    windows = {
+        proxwindow = false,
+    },
+})
+
+DXE:RegisterRealmPatch(realm, "bottrash", {
+    windows = {
+        proxwindow = false,
+    },
+})
