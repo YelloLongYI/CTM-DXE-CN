30a367f962f77b9841e64d9060a66fb3b81d2eb8
yilhuang <hyl88369391@163.com>
2026-08-30 10:57:10 +0800

    update

--- pack-dxe.bat
+++ pack-dxe.bat
@@ -1,5 +1,5 @@
 @echo off
-setlocal
+setlocal enabledelayedexpansion
 cd /d "%~dp0"
 
 set "RAR=C:\Program Files\WinRAR\Rar.exe"
@@ -12,8 +12,11 @@ if not exist "%RAR%" (
 echo Deleting existing archive (if any)...
 if exist "CTM-DXE-CN.rar" del /q "CTM-DXE-CN.rar"
 
-echo Packaging all DXE* folders (excluding .kilo, tools) into CTM-DXE-CN.rar ...
-"%RAR%" a -ep1 -r -o+ -m5 -x".kilo\*" -x"tools\*" "CTM-DXE-CN.rar" DXE*
+set "LIST="
+for /d %%D in (DXE*) do set "LIST=!LIST! "%%D""
+
+echo Packaging DXE folders (excluding .kilo, tools) into CTM-DXE-CN.rar ...
+"%RAR%" a -ep1 -r -o+ -m5 -x".kilo\*" -x"tools\*" "CTM-DXE-CN.rar" %LIST%
 
 echo.
 if errorlevel 1 (
